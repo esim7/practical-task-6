729e506f5e630a8f835118d0b1c1446fdae5c6e5
esim7 <makishev@yahoo.com>
2019-09-02 16:41:39 +0600

    добавлен класс rangeofarray

--- practical-task-6/Program.cs
+++ practical-task-6/Program.cs
@@ -10,6 +10,19 @@ namespace practical_task_6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите минимальный элемент для массива RangeOfArray");
+            int minElement = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальный элемент для массива RangeOfArray");
+            int maxElement = int.Parse(Console.ReadLine());
+
+            RangeOfArray mas = new RangeOfArray(minElement, maxElement);
+
+            for(int i = mas.MinIndex; i < mas.MaxIndex; i++)
+            {
+                mas[i] = i;
+                Console.WriteLine("элемент массива под индексом[{0}] - {1}", i, mas[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
--- practical-task-6/RangeOfArray.cs
+++ practical-task-6/RangeOfArray.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace practical_task_6
 {
-    class RangeOfArray
+    public class RangeOfArray
     {
+        private int[] _array;
+        public int _size { get; set; }
+        private int _minIndex;
+        private int _maxIndex;
+
+        public RangeOfArray(int minIndex, int maxIndex)
+        {
+            _minIndex = minIndex;
+            _maxIndex = maxIndex;
+            _size = _maxIndex - _minIndex;
+            _array = new int[_size];
+        }
+
+        public int MinIndex
+        {
+            get { return _minIndex; }
+            set
+            {
+                if (value > MaxIndex)
+                {
+                    _minIndex = value;
+                    int temp = MinIndex;
+                    MinIndex = MaxIndex;
+                    MaxIndex = temp;
+                }
+            }
+        }
+        public int MaxIndex
+        {
+            get { return _maxIndex; }
+            set
+            {
+                if (value < MinIndex)
+                {
+                    _maxIndex = value;
+                    int temp = MinIndex;
+                    MinIndex = MaxIndex;
+                    MaxIndex = temp;
+                }
+            }
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return _array[Math.Abs(i) - Math.Abs(MinIndex)];
+            }
+            set
+            {
+                _array[Math.Abs(i) - Math.Abs(MinIndex)] = value;
+            }
+        }
     }
 }
